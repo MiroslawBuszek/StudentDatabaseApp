f0c114cf191619d52b7381238200e16fcc97d190
MiroslawBuszek <zoowett@gmail.com>
2019-04-09 13:57:19 +0200

    Initial commit

---
+++ src/com/mbuszek/Student.java
@@ -0,0 +1,16 @@
+package com.mbuszek;
+
+public class Student {
+
+    // constructor: prompt user to enter student's name and year
+
+    // generate an id
+
+    // enroll in course
+
+    // view balance
+
+    // pay tuition
+
+    // show status
+}
---
+++ src/com/mbuszek/StudentDatabaseApp.java
@@ -0,0 +1,12 @@
+package com.mbuszek;
+
+public class StudentDatabaseApp {
+
+    public static void main(String[] args) {
+
+        // ask how many new users we want to add
+
+        // create n number of new students
+
+    }
+}
